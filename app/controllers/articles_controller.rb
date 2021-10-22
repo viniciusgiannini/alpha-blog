359370496c796af183e745f17bb97d7daa1ce2a0
Vinicius Giannini <vinicius.lemegiannini@gmail.com>
2021-10-22 16:12:32 -0300

    Add one-to-many associations between users and articles from backend

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -22,6 +22,7 @@ class ArticlesController < ApplicationController
   # POST /articles or /articles.json
   def create
     @article = Article.new(article_params)
+    @article.user = User.first
     if @article.save
       flash[:notice] = "Article was created successfully."
       redirect_to @article

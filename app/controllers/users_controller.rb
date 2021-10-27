146498cc92f7e8fa7a92015adc02edc8a016541d
Vinicius Giannini <vinicius.lemegiannini@gmail.com>
2021-10-27 20:20:58 -0300

    Add user index and cleanup layout

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,18 +5,12 @@ class UsersController < ApplicationController
         @articles = @user.articles
     end
 
-    def new
-        @user = User.new
+    def index
+        @users = User.all
     end
 
-    def create
-        @user = User.new(user_params)
-        if @user.save
-            flash[:notice] = "Welcome to the Alpha Blog #{@user.username}!"
-            redirect_to articles_path
-        else
-            render 'new'
-        end
+    def new
+        @user = User.new
     end
 
     def edit
@@ -27,9 +21,19 @@ class UsersController < ApplicationController
         @user = User.find(params[:id])
         if @user.update(user_params)
             flash[:notice] = "Your account was succeefully update"
-            redirect_to articles_path
+            redirect_to @user
         else
             render 'edit'
+        end
+    end
+
+    def create
+        @user = User.new(user_params)
+        if @user.save
+            flash[:notice] = "Welcome to the Alpha Blog #{@user.username}!"
+            redirect_to articles_path
+        else
+            render 'new'
         end
     end
      

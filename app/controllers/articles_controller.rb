37eab5b6243410c6c325e457237a3a85496e1229
Vinicius Giannini <vinicius.lemegiannini@gmail.com>
2021-11-01 21:33:07 -0300

    Restrict actions for users controller. Added require_same_user method to users controller.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,9 +1,11 @@
 class ArticlesController < ApplicationController
   before_action :set_article, only: [:show, :edit, :update, :destroy]
+  before_action :require_user, except: [:show, :index]
+  before_action :require_same_user, only: [:edit, :update, :destroy]
 
   # GET /articles or /articles.json
   def index
-    @articles = Article.includes(:user).paginate(page: params[:page], per_page: 50)
+    @articles = Article.includes(:user).paginate(page: params[:page], per_page: 5)
   end
 
   # GET /articles/1 or /articles/1.json
@@ -57,4 +59,12 @@ class ArticlesController < ApplicationController
     def article_params
       params.require(:article).permit(:title, :description)
     end
+
+    def require_same_user
+      if current_user != @article.user
+        flash[:alert] = "You can only edit or delete your own article"
+        redirect_to @article
+      end
+    end
+
 end
